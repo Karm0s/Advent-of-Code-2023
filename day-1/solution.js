e9e5217c5f07c05afe78a503c2cd9e0cf476de57
Karm0s <18452314+Karm0s@users.noreply.github.com>
2023-12-02 20:18:18 -0500

    Day-1: replace map and flat calls with one flatMap call.

--- day-1/solution.js
+++ day-1/solution.js
@@ -6,21 +6,20 @@ const spelled_digits = [/[1-9]/g, 'one', 'two', 'three', 'four', 'five', 'six', 
 let solution = 0;
 content.slice(0, content.length - 1).split('\n').map(line => {
   let tokens = spelled_digits
-  .map(digit => {
-    const matches = Array.from(line.matchAll(digit), match => ({value: match[0], index: match['index']}));
-    if (matches) {
-      return matches;
-    }
-  })
-  .flat()
-  .sort((a, b) => ((a.index !== b.index) ? ((a.index > b.index) ? 1 : -1) : 0))
-  .map(token => {
-    if (isNaN(token.value)) {
-      return Number(spelled_digits.indexOf(token.value));
-    }
-    return Number(token.value);
+    .flatMap(digit => {
+      const matches = Array.from(line.matchAll(digit), match => ({ value: match[0], index: match['index'] }));
+      if (matches) {
+        return matches;
+      }
+    })
+    .sort((a, b) => ((a.index !== b.index) ? ((a.index > b.index) ? 1 : -1) : 0))
+    .map(token => {
+      if (isNaN(token.value)) {
+        return Number(spelled_digits.indexOf(token.value));
+      }
+      return Number(token.value);
 
-  });
+    });
   const line_solution = Number(`${tokens[0]}${tokens.slice(-1)}`);
   solution += line_solution;
 });
