c1908674fcb9808e05a2b8eca7598ecdb9d0a454
Karm0s <18452314+Karm0s@users.noreply.github.com>
2023-12-09 22:02:21 -0500

    AoC Day 3 - Part 2: Solution using different approach.

--- day-3/solution-part1.ts
+++ day-3/solution-part1.ts
@@ -1,12 +1,14 @@
 // Read the file synchronously
 import fs from 'node:fs';
-
 const content: String = fs.readFileSync('./input', 'utf8');
 
+// Create variable to store accumulated solution
 let solution = 0;
 
+// Split the file a list of lines
 const lines = content.slice(0, content.length - 1).split('\n');
 
+// Loop through the lines
 for (let line_index = 0; line_index < lines.length; line_index++) {
 
   const line = lines[line_index];
@@ -16,66 +18,50 @@ for (let line_index = 0; line_index < lines.length; line_index++) {
   for (let char_index = 0; char_index <= line.length; char_index++) {
     if (char_index < line.length && !isNaN(parseInt(line[char_index]))) {
       number_str += line[char_index];
-      continue;
     }
     else {
 
       if (number_str) {
-        if (char_index === line.length) {
-          console.log("REACHED EOL WITH NUMBER");
-
-        }
         let is_neighbour_symbol = false;
 
         let start_index = char_index - number_str.length - 1;
-        console.log(start_index);
-
-        if (start_index < 0) {
+        if (start_index > 0) {
+          let left_char = line[start_index];
+          if (left_char !== '.' && left_char !== '\n') {
+            is_neighbour_symbol = true;
+          }
+        } else {
           start_index = 0;
         }
-        let end_index = char_index;
-        if (end_index >= line.length) {
-          end_index = char_index - 1;
-        }
 
-        if (char_index - number_str.length - 1 >= 0) {
-          let left_char = line[char_index - number_str.length - 1];
-          if (left_char !== '.' && left_char !== '\n') {
-            console.log('LEFT: ' + left_char);
-            is_neighbour_symbol = true;
-          }
-        }
-        if (char_index + 1 < line.length - 1) {
+        let end_index = char_index;
+        if (end_index < line.length) {
           let right_char = line[char_index];
           if (right_char !== '.' && right_char !== '\n') {
-            console.log('RIGHT: ' + right_char);
             is_neighbour_symbol = true;
           }
+        } else {
+          end_index = char_index - 1;
         }
 
         for (let i_lookup = start_index; i_lookup <= end_index; i_lookup++) {
           if (line_index - 1 >= 0) {
             let top_char = lines[line_index - 1][i_lookup];
             if (top_char !== '.' && top_char !== '\n') {
-              console.log('top: ' + top_char);
               is_neighbour_symbol = true;
             }
           }
           if (line_index + 1 < lines.length - 1) {
             let bottom_char = lines[line_index + 1][i_lookup];
             if (bottom_char !== '.' && bottom_char !== '\n') {
-              console.log('BOTTOM: ' + bottom_char);
               is_neighbour_symbol = true;
             }
           }
         }
 
         if (is_neighbour_symbol) {
-          console.log('INCLUDED: ' + number_str);
-
           solution += parseInt(number_str);
         }
-        console.log('\n');
         number_str = '';
       }
     }
